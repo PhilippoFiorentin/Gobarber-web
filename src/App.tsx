a6e8983b59712cc39356a0186867e219be613edc
Philippo Rodrigues <philipporodrigues@Philippos-MacBook-Pro.local>
2021-07-17 21:39:41 -0300

    Criando toasts de info, sucesso e erro para login

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToasterContainer';
 // import SignUp from './pages/SignUp';
 
 const App: React.FC = () => {
@@ -10,6 +11,8 @@ const App: React.FC = () => {
       <AuthProvider>
         <SignIn />
       </AuthProvider>
+
+      <ToastContainer />
 
       <GlobalStyle />
     </>
--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -41,8 +41,12 @@ const SignIn: React.FC = () => {
           password: data.password,
         });
       } catch (err) {
-        const errors = getValidationErrors(err);
-        formRef.current?.setErrors(errors);
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+        }
+
+        // disparar toast
       }
     },
     [signIn],
